0addd1df0e84642ce370688721b8b50c5f52423d
Aizenss <irsyaderpl1@gmail.com>
2024-05-19 02:50:10 +0700

    route, dll

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('LandingPage');
 });
+Route::get('/admin/home', function () {
+    return view('Admin.Dashboard');
+});
+Route::get('/admin/category', function () {
+    return view('Admin.Category.index');
+});
+Route::get('/admin/news', function () {
+    return view('Admin.News.index');
+});
