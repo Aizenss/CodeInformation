e4e1e7e78ef9f4703b8834cd7227e4911d4f6b79
Aizenss <irsyaderpl1@gmail.com>
2024-05-19 02:51:52 +0700

    home admin page

--- resources/views/Admin/Dashboard.blade.php
+++ resources/views/Admin/Dashboard.blade.php
@@ -1,1 +1,27 @@
-Dashboard
+@extends('Layout.App')
+
+@include('Layout.Sidebar')
+
+@section('content')
+    <div class="p-8 sm:ml-80">
+        <h1 class="text-medium font-bold text-black">Selamat Datang, Admin</h1>
+        <div class="grid grid-cols-4 gap-3 mt-8">
+            <div class="flex items-center gap-3 bg-[#A5C9CA] rounded-xl shadow-xl p-4">
+                <div>
+                    <i class="fa-solid fa-newspaper text-[60px]"></i>
+                </div>
+                <div>
+                    <span class="text-[30px] font-semibold">45 Berita</span>
+                </div>
+            </div>
+            <div class="flex items-center gap-3 bg-[#A5C9CA] rounded-xl shadow-xl p-4">
+                <div>
+                    <i class="fa-solid fa-layer-group text-[60px]"></i>
+                </div>
+                <div>
+                    <span class="text-[30px] font-semibold">45 Kategori</span>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
